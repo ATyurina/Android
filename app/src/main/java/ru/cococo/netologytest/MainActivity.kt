944d0239d64d17258592b1c560380bb6fa677e77
Tyurina Alena <ifus55@mail.ru>
2023-04-27 22:28:02 +0300

    Activity change

--- app/src/main/java/ru/cococo/netologytest/MainActivity.kt
+++ app/src/main/java/ru/cococo/netologytest/MainActivity.kt
@@ -2,10 +2,12 @@ package ru.cococo.netologytest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<ImageButton>(R.id.likes)
     }
 }
